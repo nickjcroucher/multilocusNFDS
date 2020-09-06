8c9a4df9a49ddd4a749604251628c45aec4aba01
Nicholas J Croucher <ncrouche@login-7.rcs.ic.ac.uk>
2020-09-06 22:29:21 +0100

    Remove hard coded path

--- icc_compile.sh
+++ icc_compile.sh
@@ -3,5 +3,4 @@
 module load intel-suite
 module load gsl
 
-cd /work/ncrouche/E_coli/model_fitting
 icc -std=c++11 -g -O2 -o freqDepSelect main.cpp functions.cpp -L /apps/gsl/1.8/lib/ -I /apps/gsl/1.8/include/  -lgsl -lgslcblas
