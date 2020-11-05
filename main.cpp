19dee85b685d9beabf68cd3c054121d9ba416d4a
nickjcroucher <n.croucher@imperial.ac.uk>
2020-11-05 09:50:14 +0000

    Change summary statistic calculation

--- main.cpp
+++ main.cpp
@@ -547,27 +547,34 @@ int main(int argc, char * argv[]) {
         // compare to genomes in the post-vaccine period
         if (gen > 0) {
             unsigned int gen_diff = gen-minGen;
-            if (gen_diff < samplingList->size() && (*samplingList)[gen_diff] > 0 && p.programme != "s" && p.programme != "x") {
-                int compareSamplesCheck = compareSamples(gen,minGen,(*samplingList)[gen-minGen],currentIsolates,population,accessoryLoci,scList,sampledVtScFreq,sampledNvtScFreq,sampledSeroFreq[gen-minGen],serotypeList,vtCogFittingStatsList,nvtCogFittingStatsList,strainFittingStatsList,sampleOutFile,&p);
-                // penalise simulations where simulation has been halted
-                for (unsigned int i = 0; i < vtCogFittingStatsList.size(); i++) {
-                    vtCogFittingStatsList[i]+=1.0;
-                    nvtCogFittingStatsList[i]+=1.0;
-                    strainFittingStatsList[i]+=1.0;
+            if (gen_diff < samplingList->size() && (*samplingList)[gen_diff] > 0) {
+                //if (p.programme != "s" && p.programme != "x") {
+                if (p.programme != "x") {
+                    int compareSamplesCheck = compareSamples(gen,minGen,(*samplingList)[gen-minGen],currentIsolates,population,accessoryLoci,scList,sampledVtScFreq,sampledNvtScFreq,sampledSeroFreq[gen-minGen],serotypeList,vtCogFittingStatsList,nvtCogFittingStatsList,strainFittingStatsList,sampleOutFile,&p);
+                    if (compareSamplesCheck != 0) {
+                        std::cerr << "Unable to compare simulated and actual frequencies" << std::endl;
+                        usage(argv[0]);
+                        return 1;
+                    } else {
+                        numberComparisons++;
+                    }
+                } else if (p.programme == "f" || p.programme == "b") {
+                    int justRecordStatsCheck = justRecordStats(gen,minGen,(*samplingList)[gen-minGen],currentIsolates,accessoryLoci);
+                    if (justRecordStatsCheck != 0) {
+                        std::cerr << "Unable to record simulation statistics" << std::endl;
+                        usage(argv[0]);
+                        return 1;
+                    } else {
+                        numberComparisons++;
+                    }
                 }
-                if (compareSamplesCheck != 0) {
-                    std::cerr << "Unable to compare simulated and actual frequencies" << std::endl;
-                    usage(argv[0]);
-                    return 1;
-                } else {
-                    numberComparisons++;
-                }
-            } else if (gen_diff < samplingList->size() && (*samplingList)[gen_diff] > 0 && p.programme == "s") {
-                int justRecordStatsCheck = justRecordStats(gen,minGen,(*samplingList)[gen-minGen],currentIsolates,accessoryLoci);
-                if (justRecordStatsCheck != 0) {
-                    std::cerr << "Unable to record simulation statistics" << std::endl;
-                    usage(argv[0]);
-                    return 1;
+                if (!continue_reproducing) {
+                    // penalise simulations where simulation has been halted
+                    for (unsigned int i = 0; i < vtCogFittingStatsList.size(); i++) {
+                        vtCogFittingStatsList[i]+=1.0;
+                        nvtCogFittingStatsList[i]+=1.0;
+                        strainFittingStatsList[i]+=1.0;
+                    }
                 }
             }
         }
